3f19ffe018907a7dcc51238cbd8d25449b2fc59b
celebron <celebron.ru@gmail.com>
2022-01-17 01:23:14 +0300

    Изменение механизма регистрации классов social

--- src/SocialConfiguration.php
+++ src/SocialConfiguration.php
@@ -10,6 +10,33 @@ class SocialConfiguration extends Component
 {
     public string $route = "site/social";
 
-    public array $socials = [];
+    private array $_socials = [];
+    private array $_links = [];
+    public function getSocials(): array
+    {
+        return $this->_socials;
+    }
+    public function getLinks(): array
+    {
+        return $this->_links;
+    }
+
+    public function setSocials(array $value): void
+    {
+        foreach ($value as $key=>$di) {
+            if(is_numeric($key)) {
+                $reflection = new \ReflectionClass($di['class']);
+                $key = strtolower($reflection->getShortName());
+            }
+            $this->_socials[$key] = $di;
+            $classname = $di['class'];
+            $this->_links[$key] = $classname::Url();
+        }
+    }
+
+    public function init ()
+    {
+
+    }
 
 }
