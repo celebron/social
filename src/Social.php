f06f6c171913c546ca07eee87810edffda359a23
celebron <celebron.ru@gmail.com>
2023-01-14 12:54:52 +0300

    delete use session

--- src/Social.php
+++ src/Social.php
@@ -261,16 +261,7 @@ abstract class Social extends Model
     public function loginSuccess(SocialController $action): mixed
     {
         $eventArgs = new SuccessEventArgs($action);
-        $eventArgs->useSession = $this->useSession;
         $this->trigger(self::EVENT_LOGIN_SUCCESS, $eventArgs);
-        if($eventArgs->useSession) {
-            if(!\Yii::$app->session->isActive) {
-                \Yii::$app->session->open();
-            }
-            $session = \Yii::$app->session;
-            \Yii::debug('Used session to save token', static::class);
-            $session[static::socialName() . '.token'] = $this->data['token'];
-        }
         return $eventArgs->result ?? $action->goBack();
     }
 
