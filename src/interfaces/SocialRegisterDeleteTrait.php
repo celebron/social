814d6bb033193b27d05f5fcf959c8e798a10716e
celebron <celebron.ru@gmail.com>
2023-07-23 20:52:00 +0300

    fix

--- src/interfaces/SocialRegisterDeleteTrait.php
+++ src/interfaces/SocialRegisterDeleteTrait.php
@@ -2,24 +2,23 @@
 
 namespace Celebron\social\interfaces;
 
-use Celebron\social\attrs\SocialRequest;
+use Celebron\social\Response;
 use Celebron\social\SocialResponse;
 trait SocialRegisterDeleteTrait
 {
     /**
      * @throws \Exception
      */
-    public function socialRegister(SocialResponse $response):bool
+    public function socialRegister(SocialResponse $response):Response
     {
-        return $response->saveModel($this, false);
+        return Response::saveModel($response, $this, false);
     }
 
     /**
      * @throws \Exception
      */
-    #[SocialRequest(false)]
-    public function socialDelete(SocialResponse $response):bool
+    public function socialDelete(SocialResponse $response):Response
     {
-        return $response->saveModel($this, true);
+        return Response::saveModel($response, $this, true);
     }
 }
