e6a37bacf973e163ca0c43250088d50154846e07
celebron <celebron.ru@gmail.com>
2023-07-25 14:43:46 +0300

    Дев режим (временно)

--- src/behaviors/SocialViewBehavior.php
+++ src/behaviors/SocialViewBehavior.php
@@ -10,17 +10,5 @@ use Celebron\social\Configuration;
  */
 class SocialViewBehavior extends Behavior
 {
-    private bool $_active = false;
 
-    /**
-     * @return bool
-     */
-    public function getActive (): bool
-    {
-        return $this->params['active'] ?? $this->_active;
-    }
-    public function setActive (bool $value): void
-    {
-        $this->_active = $value;
-    }
 }
