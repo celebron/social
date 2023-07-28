883eaa13317c532ba2667f3cbe1a9cafae95d4ed
celebron <celebron.ru@gmail.com>
2023-07-28 18:06:17 +0300

    Русификация

---
+++ src/messages/social.php
@@ -0,0 +1,7 @@
+<?php
+return  [
+   "Social '{socialName}' not found" => "Соц. сеть '{socialName}' не зарегистрирована в системе",
+   "Social '{socialName}' not active" => "Соц. сеть '{socialName}' деактивирована",
+   "Authorized" => "Авторизована",
+   "Not authorized" => "Не авторизована",
+];
