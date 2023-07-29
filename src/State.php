0896f3c5c2c672041ef23c6aad46245ad4e8ddf4
celebron <celebron.ru@gmail.com>
2023-07-29 11:41:37 +0300

    fix

--- src/State.php
+++ src/State.php
@@ -3,8 +3,10 @@
 namespace Celebron\social;
 
 use yii\base\Exception;
+use yii\base\InvalidArgumentException;
 use yii\base\UnknownMethodException;
 use yii\helpers\Json;
+use yii\helpers\StringHelper;
 
 /**
  * Создания State параметра для запроса в OAuth2 server
@@ -18,7 +20,7 @@ class State implements \Stringable
     public const METHOD_REGISTER = 'register';
     public const METHOD_DELETE = 'delete';
 
-    public ?string $method = null;
+    public ?string $action = null;
     public string $random;
     public ?string $state = null;
 
@@ -27,40 +29,26 @@ class State implements \Stringable
      */
     protected function __construct ()
     {
-        $this->random = \Yii::$app->security->generateRandomString();
+        $this->random = self::generateRandomString(10);
     }
 
     protected function decode($stateBase64): self
     {
-        $data = Json::decode(base64_decode($stateBase64));
-        $this->method = $data['m'];
+        $data = Json::decode(StringHelper::base64UrlDecode($stateBase64));
+        $this->action = $data['a'];
         $this->state = $data['s'];
         $this->random = $data['r'];
         return $this;
     }
 
-    public function normalizeMethod():string
-    {
-        if(str_contains($this->method, '-')) {
-            $split = [];
-            foreach (explode('-', $this->method) as $exp) {
-                $split[] = ucfirst($exp);
-            }
-            $method = implode($split);
-        } else {
-            $method = ucfirst($this->method);
-        }
-        return  $method;
-    }
-
     protected function encode(): string
     {
         $data = [
-            'm' => $this->method,
+            'a' => $this->action,
             's' => $this->state,
             'r' => $this->random,
         ];
-        return base64_encode(Json::encode($data));
+        return StringHelper::base64UrlEncode(Json::encode($data));
     }
 
     public function equalRandom(null|string|self $random): bool
@@ -71,24 +59,23 @@ class State implements \Stringable
         return $this->random === $random;
     }
 
+    public function normalizeMethod():string
+    {
+        if(str_contains($this->action, '-')) {
+            $split = [];
+            foreach (explode('-', $this->action) as $exp) {
+                $split[] = ucfirst($exp);
+            }
+            $method = implode($split);
+        } else {
+            $method = ucfirst($this->action);
+        }
+        return  $method;
+    }
+
     public function equalAction(string $action):bool
     {
-        return $this->method === $action;
-    }
-
-    public function isLogin():bool
-    {
-        return $this->equalAction(self::METHOD_LOGIN);
-    }
-
-    public function isRegister():bool
-    {
-        return $this->equalAction(self::METHOD_REGISTER);
-    }
-
-    public function isDelete():bool
-    {
-        return $this->equalAction(self::METHOD_DELETE);
+        return $this->action === $action;
     }
 
     public function __toString ()
@@ -96,10 +83,10 @@ class State implements \Stringable
         return $this->encode();
     }
 
-    public static function create(string $method, ?string $state = null):self
+    public static function create(string $action, ?string $state = null):self
     {
         $obj = new self();
-        $obj->method = strip_tags($method);
+        $obj->action = strip_tags($action);
         $obj->state = strip_tags($state);
         return $obj;
     }
@@ -109,15 +96,32 @@ class State implements \Stringable
         return (new self())->decode($stateBase64);
     }
 
+    public static function generateRandomString(int $length = 32):string
+    {
+        if ($length < 1) {
+            throw new InvalidArgumentException('First parameter ($length) must be greater than 0');
+        }
+        $bytes = random_bytes($length);
+        return substr(StringHelper::base64UrlEncode($bytes), 0, $length);
+    }
+
     public static function __callStatic ($methodName, $arguments)
     {
-        $prefix = 'start';
-        $prefixLen = strlen($prefix);
-
-        if(str_starts_with($methodName, $prefix)) {
-            return static::create(substr($methodName, $prefixLen), $arguments[0] ?? null);
+        $prefix = 'create';
+        if(StringHelper::startsWith($methodName, $prefix)) {
+            $name = strtolower(substr($methodName, strlen($prefix)));
+            return static::create($name, $arguments[0] ?? null);
         }
         throw new UnknownMethodException('Calling unknown method: ' . static::class . "::$methodName()");
+    }
+
+    public function __call ($methodName, $arguments)
+    {
+        $isPrefix = 'is';
+        if(StringHelper::startsWith($methodName, $isPrefix)) {
+            $name = strtolower(substr($methodName, strlen($isPrefix)));
+            return $this->equalAction($name);
+        }
     }
 
 }
