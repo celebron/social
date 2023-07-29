2891170bbe540046869ba1027e51b4491037b59c
celebron <celebron.ru@gmail.com>
2023-07-29 10:07:07 +0300

    Изменения отображения формы менеджмента

--- src/widgets/views/managementOne.php
+++ src/widgets/views/managementOne.php
@@ -16,25 +16,43 @@ $statusPlay = Html::a("<i class='bi bi-play'></i>", $social->url('register'));
 $statusStop = "<i class='bi bi-stop'></i> " . \Yii::t('social', 'Not authorized');
 $statusRegistered = Html::a("<i class='bi bi-toggle2-on'></i>", $social->url('delete'));
 $statusDeleted = Html::a("<i class='bi bi-toggle2-off'></i>", $social->url('register'));
-
-
 ?>
-<td class="social-name">
-    <?= $social->name ?>
-</td>
-<?php if (empty($social->getSocialId())): ?>
+
+<?php if($this->context->idView): ?>
+    <td class="social-name">
+        <?= $social->name ?>
+    </td>
+    <?php if (empty($social->getSocialId())): ?>
     <td class="social-id empty">
         <?= $statusStop ?>
     </td>
     <td class="social-manage empty">
         <?= $statusDeleted ?>
     </td>
-<?php else: ?>
+    <?php else: ?>
     <td class="social-id isset">
         <?= $statusPlay ?>
-        <?= ($this->context->idView) ? $social->getSocialId() : \Yii::t('social', 'Authorized') ?>
+        <?= $social->getSocialId() ?>
     </td>
     <td class="social-manage isset">
         <?= $statusRegistered ?>
     </td>
+    <?php endif; ?>
+<?php else: ?>
+    <?php if (empty($social->getSocialId())): ?>
+    <td class="social-name">
+        <?= $statusStop ?><?= $social->name ?>
+    </td>
+    <td class="social-manage empty">
+        <?= $statusDeleted ?>
+    </td>
+    <?php else: ?>
+    <td class="social-name">
+        <?= $statusPlay ?>
+        <?= $social->name ?>
+    </td>
+    <td class="social-manage isset">
+        <?= $statusRegistered ?>
+    </td>
+    <?php endif; ?>
 <?php endif; ?>
