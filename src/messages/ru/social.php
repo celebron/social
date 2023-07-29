384ea3adbb7ff4d5b089e62ae3fea1e121b491a7
celebron <celebron.ru@gmail.com>
2023-07-29 09:47:59 +0300

    Русификация

--- src/messages/ru/social.php
+++ src/messages/ru/social.php
@@ -2,6 +2,6 @@
 return  [
    "Social '{socialName}' not found" => "Соц. сеть '{socialName}' не зарегистрирована в системе",
    "Social '{socialName}' not active" => "Соц. сеть '{socialName}' деактивирована",
-   "Authorized" => "Авторизована",
-   "Not authorized" => "Не авторизована",
+   "Authorized" => "Авторизовано",
+   "Not authorized" => "Не авторизовано",
 ];
