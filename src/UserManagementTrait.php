10226b9311d32188b3135651a413a70806bb6190
celebron <celebron.ru@gmail.com>
2023-08-03 19:09:15 +0300

    -UserManagementBehavior - принцип не работает *UserManagementTrait - принцип из выше

--- src/UserManagementTrait.php
+++ src/UserManagementTrait.php
@@ -2,8 +2,23 @@
 
 namespace Celebron\socialSource;
 
+use yii\web\UnauthorizedHttpException;
+
 trait UserManagementTrait
 {
+    abstract public static function fieldSearch (string $field, mixed $id): ?self;
+    abstract public function getRememberTime():int;
+
+    public function socialLogin(ResponseSocial $response):bool
+    {
+        $field = $this->getSocialField($response->socialName);
+        $login = $this::fieldSearch($field, $response->getId());
+        if ($login === null) {
+            throw new UnauthorizedHttpException(\Yii::t('social', 'Not authorized'));
+        }
+        return \Yii::$app->user->login($login, $this->getRememberTime());
+    }
+
     /**
      * @throws \Exception
      */
