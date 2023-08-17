e4d95d4fbf2c181757f7a1295c93bc8e69c983e1
celebron <celebron.ru@gmail.com>
2023-08-17 12:57:31 +0300

    fix

--- widgets/SocialAsset.php
+++ widgets/SocialAsset.php
@@ -9,7 +9,7 @@ use yii\bootstrap5\BootstrapAsset;
 
 class SocialAsset extends \yii\web\AssetBundle
 {
-    public $sourcePath = '@Celebron/socialSource/widgets/public';
+    public $sourcePath = '@Celebron/widgets/social/public';
     public $css = [
         'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css',
         'social.css'
@@ -17,5 +17,4 @@ class SocialAsset extends \yii\web\AssetBundle
     public $depends = [
         BootstrapAsset::class,
     ];
-
 }
