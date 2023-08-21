ec1b85aae62141099767726c24aab5d8c033daf2
celebron <celebron.ru@gmail.com>
2023-08-21 11:31:12 +0300

    Переработка send в AbstractData.php

--- socials/Ok.php
+++ socials/Ok.php
@@ -82,7 +82,7 @@ class Ok extends OAuth2 implements UrlsInterface, ViewerInterface
         $params['access_token'] = $token;
 
         $request->post($params);
-        return $request->responseId('uid', handler: function (Response $res) {
+        return $request->responseId('uid', handler: function (Response $res):Response {
             if(isset($res->data['error_code'], $res->data['error_msg'])) {
                 throw new BadRequestHttpException(\Yii::t('social', '[{socialName}]Error {error} E{statusCode}. {description}', [
                     'socialName' => $this->socialName,
@@ -91,6 +91,7 @@ class Ok extends OAuth2 implements UrlsInterface, ViewerInterface
                     'error' => '',
                 ]));
             }
+            return $res;
         });
     }
 
